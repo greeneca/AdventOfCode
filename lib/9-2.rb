fc9490c2cb2243f777b45af3ed138b3e4ff1f56d
Charles Greene <charles.greene@redspace.com>
2023-12-09 08:56:48 -0400

    Day 9

---
+++ lib/9-2.rb
@@ -0,0 +1,25 @@
+module D9P2
+  class D9P2
+    def run(input)
+      sum = 0
+      input.each do |line|
+        seq = line.split(" ").reverse.map{|n| n.to_i}
+        sum += get_next_value(seq)
+      end
+      puts "Sum #{sum}"
+    end
+    def get_next_value(seq)
+      if seq.uniq.count == 1
+        return seq.last
+      else
+        prev = seq.shift
+        diff = seq.map do |n|
+          new = n - prev
+          prev = n
+          new
+        end
+        return seq.last + get_next_value(diff)
+      end
+    end
+  end
+end
